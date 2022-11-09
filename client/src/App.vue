3e5966b1ac4f88d0dfa66f5cd7a83804848c7781
Gefi Sanjaya <76405521+gefisanjaya@users.noreply.github.com>
2022-11-09 11:36:05 +0700

    add templates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,68 +1,18 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list-item class="justify-center">
-        <v-list-item-content>
-          <i class="fas fa-user-circle fa-6x"></i>
-          <v-list-item-title>
-            Guess
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Free Version
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list dense>
-      <v-list-item-group
-        color="primary"
-      >
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.link"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <v-icon class="mr-5" @click="drawer = !drawer">fas fa-user-circle</v-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <router-view>
-
-      </router-view>
-    </v-main>
-  </v-app>
+  <router-view />
 </template>
 
-<script>
+<style lang="scss">
+  @import 'scss/main.scss';
 
-  export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-        { title: 'Account', icon: 'mdi-account-circle-outline', link: '/about' },
-        { title: 'Login', icon: 'mdi-help-box', link: '/login' },
-        { title: 'Quotes', icon: 'mdi-format-quote-close-outline', link: '/add-post' },
-        { title: 'Settings', icon: 'mdi-cog-outline', link: '/home' },
-        { title: 'Help', icon: 'mdi-chat-question-outline', link: '/' },
-        { title: 'About', icon: 'mdi-alert-circle-outline', link: '/' },
-      ], 
-    }),
+  #app {
+    font-family: 'Source Sans Pro', Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
   }
-</script>
+
+  body {
+    margin: 0;
+  }
+</style>
